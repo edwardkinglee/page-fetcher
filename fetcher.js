45d9b8d4e4f29fd2b314005d139cd1f5689f3ac3
edwardkinglee <edward.lee3@telus.com>
2022-11-23 23:50:46 -0500

    updated fetcher.js and tested

---
+++ fetcher.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const fs = require('fs');
+const readline = require("readline");
+
+const args = process.argv.slice(2);
+
+const url = args[0];
+const fileLocation = args[1];
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log('error:', error); // Print the error if one occurred
+  }
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the Google homepage.
+
+  fs.writeFile(fileLocation, body, err => {
+    if (err) {
+      console.error(err);
+    }
+    // file written successfully
+    console.log(`Downloaded and saved bytes to ${fileLocation}`);
+    rl.close();
+  });
+
+});
